c9cd17000a0d066b0b69db0732731113859240f4
Valerij Timofeev <valerij.timofeev@trustedshops.de>
2021-03-27 00:44:55 +0100

    feat:MOD-1385 push to the public AWS ECR

--- build_for_aws_ecr.sh
+++ build_for_aws_ecr.sh
@@ -1,28 +1,24 @@
 #!/bin/sh -e
-
 export TAG=terraform14
-# ECR is located in TS prod guarantee account, later change to the new one
-export AWSUME_ACCOUNT=prod
-#export AWS_ACCOUNT_ID=636672185349
+export AWSUME_ACCOUNT=<account profile where the ECR is located>
+export REGISTRY_ALIAS=<registry alias for the public repository>
 export REGISTRY_URI=public.ecr.aws
-export REGISTRY_ALIAS=u1c8d5f2
 export AWS_REGION=us-east-1
 export IMAGE_NAME=autostaging/auto-staging-codebuild
-# get-login has been removed in cli v2 https://github.com/aws/aws-cli/issues/5014
-# https://docs.aws.amazon.com/AmazonECR/latest/userguide/Registries.html
+### build
 # Login to DockerHub if you cannot use public base image
 # docker login -u $DOCKERHUB_USER
-# login to AWS
-awsume $AWSUME_ACCOUNT
-aws sts get-caller-identity
-aws ecr-public get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $REGISTRY_URI
-# build and push the latest
 docker build -t $IMAGE_NAME ./$TAG
-
-#docker tag $IMAGE_NAME:latest $REGISTRY/$IMAGE_NAME:latest
+### push
+# login to AWS
+# https://docs.aws.amazon.com/AmazonECR/latest/public/docker-push-ecr-image.html
+# https://docs.aws.amazon.com/AmazonECR/latest/public/public-registries.html#public-registry-auth
+awsume $AWSUME_ACCOUNT
+# aws sts get-caller-identity
+aws ecr-public get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $REGISTRY_URI
+# latest tag is default
 docker push $REGISTRY_URI/$REGISTRY_ALIAS/$IMAGE_NAME
 # create tag
 docker tag $IMAGE_NAME:latest $REGISTRY_URI/$REGISTRY_ALIAS/$IMAGE_NAME:$TAG
 docker push $REGISTRY_URI/$REGISTRY_ALIAS/$IMAGE_NAME:$TAG
 docker logout $REGISTRY_URI
-# paste image URI in your CodeBuild project e.g.: public.ecr.aws/u1c8d5f2/autostaging/auto-staging-codebuild:terraform14
